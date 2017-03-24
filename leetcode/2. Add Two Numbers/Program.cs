4f6ae0b0947b13015eea5f29450cef89d8dc799f
VVoidV <lbr1994@mail.ustc.edu.cn>
2017-03-24 17:45:55 +0800

    _2.Add_Two_Numbers 一位一位加

--- leetcode/2. Add Two Numbers/Program.cs
+++ leetcode/2. Add Two Numbers/Program.cs
@@ -12,6 +12,19 @@ namespace _2.Add_Two_Numbers
         public int val;
         public ListNode next;
         public ListNode(int x) { val = x; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            ListNode cur = this;
+            do
+            {
+                sb.Append(cur.val.ToString());
+                cur = cur.next;
+            } while (cur != null);
+            char[] charArray = sb.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
     }
 
     public static class Solve
@@ -33,7 +46,7 @@ namespace _2.Add_Two_Numbers
             }
             return result;
         }
-        public  static ListNode addAddTwoNumbers(ListNode l1, ListNode l2)
+        public  static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
             ListNode ans = new ListNode(0);
             long a1 = getNum(l1);
@@ -45,6 +58,90 @@ namespace _2.Add_Two_Numbers
             {
                 cur.next = new ListNode(digit - '0');
                 cur = cur.next;
+            }
+            return ans.next;
+        }
+    }
+
+    public static class Solve2
+    {
+        
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode ans = new ListNode(0);
+            ListNode cur=ans;
+            int newDig;
+            int carry=0;
+            int sum;
+            for (; l1 != null && l2 != null; l1 = l1.next, l2 = l2.next) 
+            {
+                sum = l1.val + l2.val + carry;
+                if (sum > 9)
+                {
+                    newDig = sum - 10;
+                    carry = 1;
+                }
+                else
+                {
+                    newDig = sum;
+                    carry = 0;
+                }
+                cur.next = new ListNode(newDig);
+                cur = cur.next;
+            }
+
+            if (l1 == null && l2 != null)
+            {
+                while (l2!=null&&carry != 0)
+                {
+                    sum = l2.val + 1;
+                    if (sum > 9)
+                    {
+                        newDig = sum - 10;
+                        carry = 1;
+                    }
+                    else
+                    {
+                        newDig = sum;
+                        carry = 0;
+                    }
+                    cur.next = new ListNode(newDig);
+                    cur = cur.next;
+                    l2 = l2.next;
+                }
+                if (l2!=null)
+                {
+                    cur.next = l2;
+                }
+            }
+
+            else if (l1 != null && l2 == null)
+            {
+                while (l1 != null && carry != 0)
+                {
+                    sum = l1.val + 1;
+                    if (sum > 9)
+                    {
+                        newDig = sum - 10;
+                        carry = 1;
+                    }
+                    else
+                    {
+                        newDig = sum;
+                        carry = 0;
+                    }
+                    cur.next = new ListNode(newDig);
+                    cur = cur.next;
+                    l1 = l1.next;
+                }
+                if (l1 != null)
+                {
+                    cur.next = l1;
+                }
+            }
+            if (carry==1)
+            {
+                cur.next = new ListNode(1);
             }
             return ans.next;
         }
@@ -64,7 +161,7 @@ namespace _2.Add_Two_Numbers
         }
         static void Main(string[] args)
         {
-            Solve.addAddTwoNumbers(genList(1), genList(9999999991));
+            Solve2.AddTwoNumbers(genList(9), genList(1));
         }
     }
 }
