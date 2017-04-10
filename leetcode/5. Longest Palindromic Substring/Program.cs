1eb69c9af58f2148a8b8b3a5dea9bcce06ae4dac
VVoidV <lbr1994@mail.ustc.edu.cn>
2017-04-10 20:20:28 +0800

    _5.Longest_Palindromic_Substring

---
+++ leetcode/5. Longest Palindromic Substring/Program.cs
@@ -0,0 +1,96 @@
+﻿//Given a string s, find the longest palindromic substring in s.You may assume that the maximum length of s is 1000.
+
+//Example:
+
+//Input: "babad"
+
+//Output: "bab"
+
+//Note: "aba" is also a valid answer.
+//Example:
+
+//Input: "cbbd"
+
+//Output: "bb"
+
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Longest_Palindromic_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.LongestPalindrome2("zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir"));
+        }
+    }
+    public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {//最朴素的办法80ms处理下面这个字符串
+         // zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir
+            int curLength = s.Length;
+            while (curLength > 0)
+            {
+                string curSample;
+                int cursor = 0;
+                while (cursor + curLength <= s.Length)
+                {
+                    curSample = s.Substring(cursor, curLength);
+                    if (isPalindrome(curSample))
+                    {
+                        return curSample;
+                    }
+                    cursor++;
+                }
+                curLength--;
+            }
+            throw new Exception("unexcepted way");
+        }
+        private bool isPalindrome(string s)
+        {
+            for (int i = 0; i <= s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+/*通过假设回文字符串的中心点从字符串起始到末尾遍历，逐次增大回文串的长度来判断*/
+        private int maxLen;
+        private int startCur;
+        public string LongestPalindrome2(string s)
+        {
+            if (s.Length<2)
+            {
+                return s;
+            }
+            maxLen = 0;
+            startCur = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                expand(s, i, i);
+                expand(s, i, i + 1);
+            }
+            return s.Substring(startCur, maxLen);
+        }
+        private void expand(string s, int curLeft, int curRight)
+        {
+            while (curLeft >= 0 && curRight < s.Length && s[curRight] == s[curLeft])
+            {
+                curLeft--;
+                curRight++;
+            }
+            if (maxLen<curRight-curLeft-1)
+            {
+                maxLen = curRight - curLeft - 1;
+                startCur = curLeft+1;
+            }
+        }
+    }
+}
